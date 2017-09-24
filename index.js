114073672c468c32e7e4ee7667bd4be49a553491
zze-Software Engineering AYMAN D ext IMT/OLPS <ole.npm.17@gmail.com>
2017-09-24 11:54:38 +0200

    Initial release

---
+++ index.js
@@ -0,0 +1,48 @@
+var request = require("request");
+
+module.exports = {
+  sendImageMedia: sendImageMedia
+};
+
+function sendImageMedia(fileName, fileSize, fileUri, botId, userNumber){
+	var json = {
+	  "bot": function(){ return botId; }(),
+	  "user": function(){ return userNumber }(),
+	  "message": { "messageId":"", "message": { "contentMessage": {
+
+			"media": {
+			  "category": "IMAGE",
+			  "thumbnail": {
+				  "mimeType": "image/png",
+				  "fileSize": function(){ return fileSize }(),
+				  "fileName": function(){ return fileName }(),
+				  "fileUri": function(){ return fileUri}()
+			  },
+			  "payload": {
+				  "mimeType": "image/png",
+				  "fileSize": function(){ return fileSize }(),
+				  "fileName": function(){ return fileName }(),
+				  "fileUri": function(){ return fileUri}()
+			  }
+		  } 
+	 }}}
+	}
+
+	console.log("JSON: ", json);
+	
+	var options = { method: 'POST',
+	  url: API_BASE_URL + '/send',
+	  headers: 
+	   { 
+		authorization: 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJkIjp7InRpbWUiOjE1MDU3MjgwNDMsInVzZXIiOiIzMzY0NTMzMzgyMyJ9LCJ2IjowLCJpYXQiOjE1MDU3MjgwNDN9.IHk2yczEYaTQt3RBYKqdUZC6ABqMccu2b69qLqx0N48',
+		'content-type': 'application/json' },
+	  body: json,
+		 json: true };
+		 
+	request(options, function (error, response, body) {
+	  if (error) throw new Error(error);
+
+	  console.log(body);
+	});
+	
+}
