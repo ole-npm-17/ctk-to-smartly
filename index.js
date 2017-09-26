57c9435eb008e4b331c85cc1e64633dbc51ef8c0
zze-Software Engineering AYMAN D ext IMT/OLPS <ole.npm.17@gmail.com>
2017-09-26 15:21:23 +0200

    updating token

--- index.js
+++ index.js
@@ -1,49 +1,9 @@
-var request = require("request");
-const API_BASE_URL = 'https://abcde-ctk.kmt.orange.com/ctk/json/ctk';
+var ctk = require('ctk-to-smartly');
 
-module.exports = {
-  sendImageMedia: sendImageMedia
-};
-
-function sendImageMedia(fileName, fileSize, fileUri, botId, userNumber){
-	var json = {
-	  "bot": function(){ return botId; }(),
-	  "user": function(){ return userNumber }(),
-	  "message": { "messageId":"", "message": { "contentMessage": {
-
-			"media": {
-			  "category": "IMAGE",
-			  "thumbnail": {
-				  "mimeType": "image/png",
-				  "fileSize": function(){ return fileSize }(),
-				  "fileName": function(){ return fileName }(),
-				  "fileUri": function(){ return fileUri}()
-			  },
-			  "payload": {
-				  "mimeType": "image/png",
-				  "fileSize": function(){ return fileSize }(),
-				  "fileName": function(){ return fileName }(),
-				  "fileUri": function(){ return fileUri}()
-			  }
-		  } 
-	 }}}
-	}
-
-	console.log("JSON: ", json);
+(()=>{
 	
-	var options = { method: 'POST',
-	  url: API_BASE_URL + '/send',
-	  headers: 
-	   { 
-		authorization: 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJkIjp7InRpbWUiOjE1MDU3MjgwNDMsInVzZXIiOiIzMzY0NTMzMzgyMyJ9LCJ2IjowLCJpYXQiOjE1MDU3MjgwNDN9.IHk2yczEYaTQt3RBYKqdUZC6ABqMccu2b69qLqx0N48',
-		'content-type': 'application/json' },
-	  body: json,
-		 json: true };
-		 
-	request(options, function (error, response, body) {
-	  if (error) throw new Error(error);
-
-	  console.log(body);
-	});
 	
-}
+	ctk.sendImageMedia("RCS-menu-min.png", 249285, "https://irp-cdn.multiscreensite.com/ca143f07/dms3rep/multi/RCS+Menu-min.png",
+		"c6d138f0-09b9-45ce-b40c-63a98bb2a100", "+33645333823");
+	
+})()
